a1e9ff13a31c049ce4eeabc1b13d4d14dc2bdfe4
solarpark7346 <solarpark7346@gmail.com>
2023-02-10 12:36:14 +0900

    Baekjoon2562 최댓값

---
+++ src/Baekjoon2562.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class Baekjoon2562 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+        int max = 0;
+        int number = 0;
+        int count = 0;
+        int position = 0;
+        int[] array = new int[9];
+        List<int[]> hohoho = List.of(array);
+
+        while (true){
+            if (count > array.length-1){
+                break;
+            } else {
+                array[count] = Integer.parseInt(br.readLine());
+                number = array[count];
+                if (number > max){
+                    max = number;
+                    position = count+1;
+                } count++;
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(position);
+    }
+}
