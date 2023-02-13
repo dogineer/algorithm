555dab8d0f5cc0dec276cd997e1ab77cc0e007ee
solarpark7346 <solarpark7346@gmail.com>
2023-02-13 19:28:08 +0900

    Baekjoon11720 숫자의 합

---
+++ src/Backjoon11720.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Backjoon11720 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int getsu = Integer.parseInt(br.readLine());
+        String num = br.readLine();
+        int sum = 0;
+
+        for (int i=0; i<getsu; i++){
+            sum += num.charAt(i) - '0';
+        }
+
+        System.out.println(sum);
+    }
+}
