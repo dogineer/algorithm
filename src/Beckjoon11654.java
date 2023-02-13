cf6de3151efa8b998e07e29baf5c5f76363dca2e
solarpark7346 <solarpark7346@gmail.com>
2023-02-13 18:06:59 +0900

    Baekjoon11654 아스키보드

---
+++ src/Beckjoon11654.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Beckjoon11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char character = sc.nextLine().charAt(0);
+        int num = (int)character;
+
+        System.out.println(Integer.valueOf(character));
+    }
+}
